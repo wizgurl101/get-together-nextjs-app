d562caec10e65b400fef9a0199c8aaf04cbd6541
wizgurl101 <nb.phan@hotmail.com>
2021-06-15 11:07:35 -0600

    add functionality to add a new event to db

--- pages/new-event/index.js
+++ pages/new-event/index.js
@@ -1,8 +1,25 @@
+import { useRouter } from "next/router";
 import NewEventForm from "../../components/events/NewEventForm";
 
 const NewEventPage = () => {
-  const addEventHandler = () => {
-    console.log("add new event");
+  const router = useRouter();
+
+  const addEventHandler = async (enteredEventData) => {
+    try {
+      // send a request to the new-event js file in the api folder
+      const response = await fetch("/api/new-event", {
+        method: "POST",
+        body: JSON.stringify(enteredEventData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return <NewEventForm onAddEvent={addEventHandler} />;
